50b8b9a213de7627e7f9abc74334205ec8ac8c1b
LukeHoese <157862417+LukeHoese@users.noreply.github.com>
2024-11-13 15:51:17 -0700

    Update code.js

--- code.js
+++ code.js
@@ -9,23 +9,17 @@ function helperSum(x, lo, hi)
     {
         return 0;
     }
+    
     if(lo === hi)
     {
         return x[lo]
     }
-    if ((lo+hi)%3 <= 1)
-    {
-        var midLow = Math.floor((lo + hi) / 3);
-        var midHigh = 2 * (Math.floor((lo + hi) / 3));
-    }
-    else if ((lo+hi)%3 == 2)
-    {
-        var midLow = Math.ceil((lo + hi) / 3);
-        var midHigh = 2 * (Math.ceil((lo + hi) / 3));
-    }
+
+    var midLow = Math.floor(((2 * lo) + hi) / 3);
+    var midHigh = Math.floor((lo + (2 * hi)) / 3);
 
     var sumThird1 = helperSum(x, lo, midLow);
-    var sumThird2 = helperSum(x, midlow + 1, midHigh);
+    var sumThird2 = helperSum(x, midLow + 1, midHigh);
     var sumThird3 = helperSum(x, midHigh + 1, hi);
 
     return sumThird1 + sumThird2 + sumThird3;
