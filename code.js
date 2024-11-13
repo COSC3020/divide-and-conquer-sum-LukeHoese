d17da32af05e041d95684ea549dd8b5342a1c79c
Luke Hoese <lukehoese@gmail.com>
2024-11-13 15:21:42 -0700

    v1

--- code.js
+++ code.js
@@ -1,3 +1,22 @@
-function divideAndConquerSum(a) {
-    return a;
+function divideAndConquerSum(x) 
+{
+    var tmp = 0;
+    helperSum(x, 0, x.length - 1, tmp)
+    console.log(tmp);
+    return tmp
+}
+
+function helperSum(x, lo, hi, tmp) 
+{
+    if ((lo+hi)%3 <= 1)
+    {
+        var midLow = Math.floor((lo + hi) / 3);
+        var midHigh = 2 * (Math.floor((lo + hi) / 3));
+    }
+    else if ((lo+hi)%3 == 2)
+    {
+        var midLow = Math.ceil((lo + hi) / 3);
+        var midHigh = 2 * (Math.ceil((lo + hi) / 3));
+    }
+    tmp = helperSum(x, lo, midLow, tmp) + helperSum(x, midLow, midHigh, tmp) + helperSum(x, midHigh, hi, tmp)   
 }
